1143e4171666d13719954d1661ed937903238070
GarryG <valepos@gmail.com>
2016-09-25 14:24:55 -0400

    started Client

---
+++ NQueens/NQueens/NQueensClient.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+	
+	int N = 8;
+	int generations = 1000;
+
+	Population = Population(10 * N);
+
+	while (generations > 0) {
+		
+
+		generations--;
+	}
+
+
+}
